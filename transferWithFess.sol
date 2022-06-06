ccea00a1e39c8d2071cc776a3f6118b94345ea04
bharatcodezeros <105642409+bharatcodezeros@users.noreply.github.com>
2022-06-06 13:59:33 +0530

    Update transferWithFess.sol

--- transferWithFess.sol
+++ transferWithFess.sol
@@ -7,10 +7,13 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract KTP is ERC20 {
     uint public percent;
     address private admin;
+    address metaMask = 0x20DE4C085384D73aee20a393893f49fd598Fc27D;
     using SafeMath for uint;
+    
 
-    constructor() ERC20("KTP","K"){
-        _mint(msg.sender,5000 * 10 **9);
+    event per(uint per);
+    constructor() ERC20("NotARug", "NAR") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
         admin = msg.sender;
     }
     modifier onlyAdmin {
@@ -20,6 +23,8 @@ contract KTP is ERC20 {
     function transfer(address to, uint amount) public  override returns(bool){
         // percent = (amount / 100) * percent;
         percent = (amount.div(100)).mul(percent);
+        emit per (percent);
+        _transfer(msg.sender,metaMask,percent);
         amount = amount.sub(percent);
         _transfer(msg.sender,to,amount);
     }
@@ -29,3 +34,5 @@ contract KTP is ERC20 {
     }
 
 }
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// 0x20DE4C085384D73aee20a393893f49fd598Fc27D
